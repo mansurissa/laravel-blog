0160be8ec252dac87b027fcb471f64e16902596c
Bwanakweli Alex <bwanakweli4ever@gmail.com>
2021-10-22 15:50:19 +0200

    inital

---
+++ app/Http/Controllers/blog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\posts;
+
+class blog extends Controller
+{
+    //
+    public function blog(Request $request){
+        
+        $posts=posts::get();
+        return view('home_blog',compact('posts'));
+    }
+}
